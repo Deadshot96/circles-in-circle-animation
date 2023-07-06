1b0c6e77d92d3b2eef4cde3e4044c3cb2b359372
Bhushan Patil <bhushan1810patil@gmail.com>
2023-07-06 18:10:14 +0530

    Added basic sceleton

---
+++ circle.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Circle():
+
+    def __init__(self, x, y, level):
+        self.x = x
+        self.y = y
+        self.level = level
+        self.radius = level
+        self.level = None
+        self.prevAngle = 0
+        self.currAngle = 0
+
+
+    def draw(self, win):
+        pass
+
+
+    def animate(self):
+        pass
