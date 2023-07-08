2b7dd53ef69478d28c40106461c6013325f46b2a
Bhushan Patil <bhushan1810patil@gmail.com>
2023-07-08 18:50:13 +0530

    added a few settings

--- settings.py
+++ settings.py
@@ -7,3 +7,14 @@ Y_OFF = int((HEIGHT - GAMEWIN_HEIGHT) * 0.8)
 FPS = 60
 TITLE_FONT = "comicsansms"
 FONT_SIZE = 40
+LEVELS = {
+    0: [0, ],
+    1: [150, ],
+    2: [70, ],
+    3: [30, ],
+    4: [12, ],
+    5: [8, ],
+    6: [4],
+    7: [2],
+}
+TOTAL_FRAMES = 60
